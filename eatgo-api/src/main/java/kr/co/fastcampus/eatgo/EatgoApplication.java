5c058b962f46daeef2c9843691dc9b869f1c1268
ziro0914 <ziro0914@gmail.com>
2020-06-09 14:50:22 +0900

    666

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/EatgoApplication.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/EatgoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EatgoApplication {
     
-	public static void main(String[] args) {
+	public static void main(String[] args) {   
 		SpringApplication.run(EatgoApplication.class, args);
 	}
 
